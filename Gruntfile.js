26fd210a383c58da47974a4a0c0471c741b7cc04
Jibi Abraham <jibi.abrahm@gmail.com>
2013-08-22 12:54:25 +0530

    Added a build routine
    For production use dist/nv.ed3.min.js and css/nv.d3.css

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        // Unfortunately, the order matters
+        src: [
+          "js/d3.js", "js/nv.core.js", "js/nv.utils.js", "js/nv.tooltip.js",
+          "js/nv.legend.js", "js/nv.scatter.js", "js/nv.axis.js",
+          "js/nv.line.js", "js/nv.lineChart.js"
+        ],
+        dest: 'dist/nv.<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/nv.<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
